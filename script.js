71bcf961387317210c6fc5208973833a415fda75
aswinbennyofficial <aswinbenny.official@gmail.com>
2023-04-02 16:03:21 +0530

    calculations added

--- script.js
+++ script.js
@@ -7,32 +7,33 @@ var oil=document.getElementById("oil").value;
 var mile=document.getElementById("miles").value;
 var fa=document.getElementById("flighta").value;
 var gb=document.getElementById("flightb").value;
-var news;
-var al;
+// var news;
+// var al;
 
-if(document.getElementById('new1').checked){
-    news=0;
-}
-else{
-    news=1;
-}
+// if(document.getElementById('new1').checked){
+//     news=0;
+// }
+// else{
+//     news=1;
+// }
 
-if(document.getElementById('Al1').checked){
-    al=0;
+// if(document.getElementById('Al1').checked){
+//     al=0;
     
-}
-else{
-    al=1;
-}
+// }
+// else{
+//     al=1;
+// }
 
-Number(news);
-Number(al);
+// Number(news);
+// Number(al);
 
 
-var total=Number(elec)+Number(gas)+Number(oil)+Number(mile)+Number(fa)+Number(gb)+news+al;
+var total=Number(elec)*.085+Number(gas)*1.640+Number(oil)*2.62+Number(mile)*2.78+Number(fa)*.186+Number(gb)*.13 ;
+// +news+al;
 
 
 var mainDiv = document.getElementById("result");
-mainDiv.innerHTML = "<p><em>Your carbon emmission is: </em>"+total+"</p>";
+mainDiv.innerHTML = "<p><em>Your carbon emmission is: </em>"+total+"kg </p>";
 
 }
