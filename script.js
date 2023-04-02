0567b7e6c80d0ba59382d515626208a84f21a9fc
aswinbennyofficial <aswinbenny.official@gmail.com>
2023-04-02 11:53:25 +0530

    added forms

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-// Add JavaScript code to calculate the carbon footprint
-const electricityInput = document.getElementById("electricity");
-const gasInput = document.getElementById("gas");
-const milesInput = document.getElementById("miles");
-const calculateButton = document.getElementById("calculate");
-const resultDiv = document.getElementById("result");
 
-calculateButton.addEventListener("click", () => {
-  const electricity = electricityInput.value;
-  const gas = gasInput.value;
-  const miles = milesInput.value;
+function alerter(){
 
-  const carbonFootprint = electricity * 0.5 + gas * 12 + miles * 0.404;
-  resultDiv.textContent = `Your carbon footprint is ${carbonFootprint.toFixed(2)} metric tons of CO2 per year.`;
-});
+var electricity=document.getElementById("electricity").value;
+alert("fuck you");
+// alert("hi");
+}
